35447968b898d76193ba95cfcadbb5cc31c13c2d
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-02-08 13:19:31 +0100

    Added table name property

--- system/models/Zeapps_configs.php
+++ system/models/Zeapps_configs.php
@@ -2,4 +2,5 @@
 
 class Zeapps_configs extends ZeModel
 {
+    protected $_table_name = 'zeapps_configs';
 }
--- system/models/Zeapps_modules.php
+++ system/models/Zeapps_modules.php
@@ -2,4 +2,5 @@
 
 class Zeapps_modules extends ZeModel
 {
+    protected $_table_name = 'zeapps_modules';
 }
