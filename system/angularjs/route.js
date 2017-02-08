564695c73d89c65f083152c0c8b1a097523bfaa4
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-02-08 10:26:13 +0100

    Handling page refresh w/o 404

--- system/angularjs/route.js
+++ system/angularjs/route.js
@@ -2,10 +2,28 @@ app.config(['$routeProvider',
     function ($routeProvider) {
         $routeProvider
 
+            .when('/', {
+                redirectTo: '/ng/com_zeapps/config'
+            })
+
+            .when('/ng/', {
+                redirectTo: '/ng/com_zeapps/config'
+            })
+
+            .when('/ng/com_zeapps/config', {
+                templateUrl: '/ze-apps/config/',
+                controller: 'ComZeAppsConfigCtrl'
+            })
+
             .when('/zeapps/logout', {
                 controller: 'ComZeAppsLogoutCtrl',
-                templateUrl: '/assets/index.html',
+                templateUrl: '/assets/index.html'
             })
+
+            .otherwise({
+                redirectTo : '/'
+            })
+
         ;
     }]);
 
