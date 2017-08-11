fb7a19aae1aa0eb9937b710fd0a4f071a4a99088
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-08-11 16:18:01 +0200

    added new functions to fit our needs

--- system/angularjs/factory.js
+++ system/angularjs/factory.js
@@ -3,9 +3,23 @@ app.config(["$provide",
 		$provide.decorator("zeHttp", function($delegate){
 			var zeHttp = $delegate;
 
-			zeHttp.hooks = {
-				get_all : getAll_hooks
-			};
+			zeHttp.app = {
+				hooks : {
+                    all : getAll_hooks
+                },
+				groups : {
+					all : getAll_groups,
+					post : post_groups,
+					del : delete_groups
+				},
+				user : {
+					get : get_user,
+					get_context : getContext_user,
+					all : getAll_user,
+					post : post_user,
+					del : delete_user
+				}
+            };
 
 			zeHttp.config = angular.extend(zeHttp.config || {}, {
 				save : save_config
@@ -13,10 +27,40 @@ app.config(["$provide",
 
 			return zeHttp;
 
+			// HOOKS
 			function getAll_hooks(){
 				return zeHttp.get("/zeapps/hooks/get_all");
 			}
 
+			// GROUPS
+			function getAll_groups(){
+                return zeHttp.get("/zeapps/group/all");
+			}
+			function post_groups(data){
+                return zeHttp.post("/zeapps/group/save", data);
+			}
+			function delete_groups(id){
+                return zeHttp.delete("/zeapps/group/delete/" + id);
+			}
+
+			// USER
+			function get_user(id){
+                return zeHttp.get("/zeapps/user/get/" + id);
+			}
+			function getContext_user(){
+                return zeHttp.get("/zeapps/user/get_context/");
+			}
+			function getAll_user(){
+                return zeHttp.get("/zeapps/user/all");
+			}
+			function post_user(data){
+                return zeHttp.post("/zeapps/user/save", data);
+			}
+			function delete_user(id){
+                return zeHttp.delete("/zeapps/user/delete/" + id);
+			}
+
+			// CONFIG
 			function save_config(data){
 				return zeHttp.post("/zeapps/config/save", data);
 			}
