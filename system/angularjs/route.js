0b2ebe5f7e4f4094b6f697cdca2d115d61929c3e
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-08-11 16:22:24 +0200

    updated routes + comments

--- system/angularjs/route.js
+++ system/angularjs/route.js
@@ -2,22 +2,23 @@ app.config(["$routeProvider",
 	function ($routeProvider) {
 		$routeProvider
 
+			// DEFAULT
 			.when("/", {
 				redirectTo: "/ng/com_zeapps_contact/companies/"
 			})
-
 			.when("/ng/", {
 				redirectTo: "/ng/com_zeapps/config"
 			})
 
+            // CONFIG
 			.when("/ng/com_zeapps/config", {
 				templateUrl: "/zeapps/config/",
 				controller: "ComZeAppsConfigCtrl"
 			})
 
-
+            // USERS
 			.when("/ng/com_zeapps/users", {
-				templateUrl: "/zeapps/user/",
+				templateUrl: "/zeapps/user/view",
 				controller: "ComZeAppsUsersCtrl"
 			})
 			.when("/ng/com_zeapps/users/view/:id?", {
@@ -25,45 +26,39 @@ app.config(["$routeProvider",
 				controller: "ComZeAppsUsersFormCtrl"
 			})
 
-
+			// GROUPS
 			.when("/ng/com_zeapps/groups", {
-				templateUrl: "/zeapps/group/",
+				templateUrl: "/zeapps/group/view",
 				controller: "ComZeAppsGroupsCtrl"
 			})
-			.when("/ng/com_zeapps/groups/view/:id?", {
-				templateUrl: "/zeapps/group/form ",
-				controller: "ComZeAppsGroupsFormCtrl"
-			})
 
-
-
+			// PROFILE
 			.when("/ng/com_zeapps/profile/view", {
 				templateUrl:"/zeapps/profile/view",
 				controller: "ComZeAppsProfileViewCtrl"
 			})
-
 			.when("/ng/com_zeapps/profile/edit", {
 				templateUrl:"/zeapps/profile/form",
 				controller: "ComZeAppsProfileFormCtrl"
 			})
-
 			.when("/ng/com_zeapps/profile/notifications", {
 				templateUrl:"/zeapps/profile/notifications",
 				controller: "ComZeAppsProfileNotificationsCtrl"
 			})
 
-
-
+			// MODULES
 			.when("/ng/com_zeapps/modules", {
 				templateUrl:"/zeapps/modules/",
 				controller: "ComZeAppsModulesCtrl"
 			})
 
+			// LOGOUT
 			.when("/ng/com_zeapps/logout", {
 				controller: "ComZeAppsLogoutCtrl",
 				templateUrl: "/assets/index.html"
 			})
 
+			// 404
 			.otherwise({
 				redirectTo : "/ng/com_zeapps_contact/companies/"
 			})
