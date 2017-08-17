fdb6b2a06cf26fa125b665439062b3eddeaa846a
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-08-17 11:57:20 +0200

    returning the groups of a user return the rights of said groups

--- system/models/Zeapps_user_groups.php
+++ system/models/Zeapps_user_groups.php
@@ -3,4 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Zeapps_user_groups extends ZeModel
 {
+    public function all($where = array()){
+        $where['zeapps_user_groups.deleted_at'] = null;
+
+        return $this->database()->select('*')
+                ->join('zeapps_groups', 'zeapps_groups.id = zeapps_user_groups.id_group', 'LEFT')
+                ->where($where)
+                ->where_not(array('zeapps_user_groups.id' => null))
+                ->group_by('zeapps_user_groups.id')
+                ->table('zeapps_user_groups')
+                ->result();
+    }
 }
