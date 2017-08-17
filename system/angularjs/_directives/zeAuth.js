2ba97724cd5fc11f7aea66d78f5b3ab18c10b713
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-08-17 11:50:28 +0200

    updated zeAuth directive against async issues

--- system/angularjs/_directives/zeAuth.js
+++ system/angularjs/_directives/zeAuth.js
@@ -3,19 +3,33 @@ app.directive("zeAuth", function($rootScope){
 	return {
 		restrict: "A",
 		scope: {
-            zeAuth: '='
+            zeAuth: '@'
 		},
 		link: function(scope, elm){
+            elm.hide();
+
 			scope.$watch("zeAuth", function(value){
-				evaluateRight(value, elm);
+                if(value) {
+                    evaluateRight(value, elm);
+                }
 			}, true);
+            $rootScope.$watch("user", function(value){
+                if(value) {
+                    evaluateRight(scope.zeAuth, elm);
+                }
+            }, true);
 		}
 	};
 
 	function evaluateRight(right, elm){
-		if($rootScope.user && $rootScope.user.rights && $rootScope.user.rights[right] !== "1"){
-			elm.remove();
-		}
+        if($rootScope.user && $rootScope.user.rights){
+            if($rootScope.user.rights[right] !== 1) {
+                elm.remove();
+            }
+            else{
+                elm.show();
+            }
+        }
 	}
 
 });
