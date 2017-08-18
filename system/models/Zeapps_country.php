5fcf1e8a3ca3cb1ac2ecb26dd9df2dfb18cf49f4
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-08-18 09:24:44 +0200

    added limit and offset options to the countries retrieval function

--- system/models/Zeapps_country.php
+++ system/models/Zeapps_country.php
@@ -2,9 +2,10 @@
 
 class Zeapps_country extends ZeModel
 {
-    public function all($where = array())
+    public function all($where = array(), $limit = 15, $offset = 0)
     {
         return $this->database()->select('*')
+            ->limit($limit, $offset)
             ->join(
                 'zeapps_country_lang',
                 'zeapps_country.id = zeapps_country_lang.id_country',
