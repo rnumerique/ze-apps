cb1b452d0f747d21b9c7289f41ae78a0daa88ed6
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-08-18 09:26:42 +0200

    renamed zeModalselection into zeModalsearch

--- system/angularjs/core/modalSearch.js
+++ system/angularjs/core/modalSearch.js
@@ -3,12 +3,12 @@ listModuleModalFunction.push({
     module_name:"com_zeapps_core",
     function_name:"search_modal",
     templateUrl:"/zeapps/directives/search_modal",
-    controller:"ZeAppsCoreModalSelectionCtrl",
+    controller:"ZeAppsCoreModalSearchCtrl",
     size:"lg",
     resolve:{}
 });
 
-app.controller("ZeAppsCoreModalSelectionCtrl", function($scope, $uibModalInstance, option) {
+app.controller("ZeAppsCoreModalSearchCtrl", function($scope, $uibModalInstance, option) {
 
     $scope.title = option.title || "Sélection" ;
     $scope.page = 1;
@@ -23,7 +23,7 @@ app.controller("ZeAppsCoreModalSelectionCtrl", function($scope, $uibModalInstanc
 
     function loadItems() {
     	var offset = ($scope.page - 1) * $scope.pageSize;
-        option.http.apply(undefined, [$scope.pageSize, offset]).then(function (response) {
+        option.http($scope.pageSize, offset).then(function (response) {
             if (response.data && response.data != "false") {
             	$scope.items = response.data.data;
             	$scope.total = response.data.total;
