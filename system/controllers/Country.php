219201754399a7a31f458c6d5ec0f663f290455a
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-09-01 17:03:48 +0200

    new function get_all

--- system/controllers/Country.php
+++ system/controllers/Country.php
@@ -3,6 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Country extends ZeCtrl
 {
+    public function get_all()
+    {
+        $this->load->model("Zeapps_country", "country");
+
+        if(!$countries = $this->country->all()){
+            $countries = [];
+        }
+
+        echo json_encode(array(
+            "countries" => $countries
+        ));
+    }
     public function modal($limit = 15, $offset = 0)
     {
         $this->load->model("Zeapps_country", "country");
@@ -20,6 +32,9 @@ class Country extends ZeCtrl
             $countries = [];
         }
 
-        echo json_encode(array("data" => $countries, "total" => $total));
+        echo json_encode(array(
+            "data" => $countries,
+            "total" => $total
+        ));
     }
 }
