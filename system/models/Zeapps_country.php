67fadb560a994ed0d30ce6f02aae0b2695ac5a84
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-09-22 14:44:11 +0200

    added left join with country_lang

--- system/models/Zeapps_country.php
+++ system/models/Zeapps_country.php
@@ -2,6 +2,19 @@
 
 class Zeapps_country extends ZeModel
 {
+    public function get($where = array())
+    {
+        return $this->database()->select('*')
+            ->join(
+                'zeapps_country_lang',
+                'zeapps_country.id = zeapps_country_lang.id_country',
+                'left'
+            )
+            ->where($where)
+            ->table('zeapps_country')
+            ->result();
+    }
+
     public function all($where = array(), $limit = 2147483647, $offset = 0)
     {
         return $this->database()->select('*')
